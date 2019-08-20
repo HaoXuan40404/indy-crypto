7ffc523c231eefbc8c44a5f197ea51f77b99f6ba
Kyon <kyonGuo@163.com>
2019-08-20 19:58:23 +0800

    remove useless files for selective disclosure

--- libindy-crypto/src/ffi/ctypes.rs
+++ libindy-crypto/src/ffi/ctypes.rs
@@ -25,34 +25,6 @@ impl CTypesUtils {
     }
 }
 
-macro_rules! check_useful_c_byte_array {
-    ($ptr:ident, $len:expr, $err1:expr, $err2:expr) => {
-        if $ptr.is_null() {
-            return $err1
-        }
-
-        if $len <= 0 {
-            return $err2
-        }
-
-        let $ptr = unsafe { slice::from_raw_parts($ptr, $len) };
-    }
-}
-
-macro_rules! check_useful_opt_c_byte_array {
-    ($ptr:ident, $len:expr, $err1:expr, $err2:expr) => {
-        if !$ptr.is_null() && $len <= 0 {
-            return $err2
-        }
-
-        let $ptr = if $ptr.is_null() {
-            None
-        } else {
-            unsafe { Some(slice::from_raw_parts($ptr, $len)) }
-        };
-    }
-}
-
 macro_rules! check_useful_c_reference {
     ($ptr:ident, $type:ty, $err:expr) => {
         if $ptr.is_null() {
@@ -80,24 +52,6 @@ macro_rules! check_useful_opt_c_reference {
         } else {
             Some(unsafe { & *($ptr as *const $type) })
         };
-    }
-}
-
-macro_rules! check_useful_c_reference_array {
-    ($ptrs:ident, $ptrs_len:ident, $type:ty, $err1:expr, $err2:expr) => {
-        if $ptrs.is_null() {
-            return $err1
-        }
-
-        if $ptrs_len <= 0 {
-            return $err2
-        }
-
-        let $ptrs: Vec<&$type> =
-            unsafe { slice::from_raw_parts($ptrs, $ptrs_len) }
-                .iter()
-                .map(|ptr| unsafe { &*(*ptr as *const $type) })
-                .collect();
     }
 }
 
--- libindy-crypto/src/lib.rs
+++ libindy-crypto/src/lib.rs
@@ -1,8 +1,3 @@
-#[cfg(feature = "wasm")]
-extern crate wasm_bindgen;
-#[cfg(feature = "wasm")]
-extern crate console_error_panic_hook;
-
 #[cfg(feature = "pair_amcl")]
 extern crate amcl;
 extern crate env_logger;
@@ -43,7 +38,6 @@ extern crate time;
 #[cfg(feature = "cl")]
 #[macro_use]
 pub mod cl;
-pub mod bls;
 
 #[cfg(feature = "bn_openssl")]
 #[path = "bn/openssl.rs"]
@@ -59,6 +53,3 @@ pub mod pair;
 
 #[macro_use]
 extern crate lazy_static;
-
-#[cfg(feature = "wasm")]
-pub mod wasm;
